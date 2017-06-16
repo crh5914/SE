27b24c8fa1a896359d832867d1c2d1e7660ec702
crh5914 <249683003@qq.com>
2017-06-16 09:27:08 +0800

    '微信接入文件'

---
+++ index.php
@@ -0,0 +1,87 @@
+<?php
+header('Content-type:text');
+define("TOKEN", "helper");
+$wechatObj = new wechatCallbackapiTest();
+if (isset($_GET['echostr'])) {
+    $wechatObj->valid();
+}else{
+    $wechatObj->responseMsg();
+}
+
+class wechatCallbackapiTest
+{
+    public function valid()
+    {
+        $echoStr = $_GET["echostr"];
+        if($this->checkSignature()){
+            header('content-type:text');
+            echo $echoStr;
+            exit;
+        }
+    }
+
+    private function checkSignature()
+    {
+        $signature = $_GET["signature"];
+        $timestamp = $_GET["timestamp"];
+        $nonce = $_GET["nonce"];
+
+        $token = TOKEN;
+        $tmpArr = array($token, $timestamp, $nonce);
+        sort($tmpArr, SORT_STRING);
+        $tmpStr = implode( $tmpArr );
+        $tmpStr = sha1( $tmpStr );
+
+        if( $tmpStr == $signature ){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public function responseMsg()
+    {
+        $postStr = file_get_contents('php://input');
+        if (!empty($postStr)){
+            $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+            $fromUsername = $postObj->FromUserName;
+            $toUsername = $postObj->ToUserName;
+            $keyword = trim($postObj->Content);
+            $time = time();
+            $textTpl = "<xml>
+                        <ToUserName><![CDATA[%s]]></ToUserName>
+                        <FromUserName><![CDATA[%s]]></FromUserName>
+                        <CreateTime>%s</CreateTime>
+                        <MsgType><![CDATA[%s]]></MsgType>
+                        <Content><![CDATA[%s]]></Content>
+                        <FuncFlag>0</FuncFlag>
+                        </xml>";
+            if($keyword == "?" || $keyword == "？")
+            {
+                $msgType = "text";
+                $contentStr = 'http://183.3.139.134:6464/app/#/login';
+                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+                echo $resultStr;
+            }
+			$linkTpl = "<xml>
+						<ToUserName><![CDATA[%s]]></ToUserName>
+						<FromUserName><![CDATA[%s]]></FromUserName>
+						<CreateTime>%s</CreateTime>
+						<MsgType><![CDATA[link]]></MsgType>
+						<Title><![CDATA[database]]></Title>
+						<Description><![CDATA[manager system]]></Description>
+						<Url><![CDATA[http://115.159.211.193:8080/database/index.jsp]]></Url>
+						<MsgId>123456</MsgId>
+						</xml>";
+			if(trim($keyword)=='link'){
+				$responseStr = sprintf($linkTpl,$fromUsername,$toUsername,date("Y-m-d H:i:s",time()));
+				echo $responseStr;
+			}
+			
+        }else{
+            echo "";
+            exit;
+        }
+    }
+}
+?>
