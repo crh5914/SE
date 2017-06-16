afd30b031be1ff9836b5eec57ec17b9cac1aadd8
BigWolf <zhouyuliang123@hotmail.com>
2017-06-16 21:03:55 +0800

    位置选择器

---
+++ src/js/city.data.js
@@ -0,0 +1,65 @@
+var cityData = [{
+	value: '100',
+	text: '芳草园小区',
+	children: [{
+		value: "101",
+		text: "1栋"
+	}, {
+		value: "102",
+		text: "2栋"
+	}, {
+		value: "103",
+		text: "3栋"
+	}, {
+		value: "104",
+		text: "4栋"
+	}]
+}, {
+	value: '200',
+	text: '珠江俊园',
+	children: [{
+		value: "201",
+		text: "1栋"
+	}, {
+		value: "202",
+		text: "2栋"
+	}, {
+		value: "203",
+		text: "3栋"
+	}, {
+		value: "204",
+		text: "4栋"
+	}]
+}, {
+	value: '300',
+	text: '海富花园',
+	children: [{
+		value: "301",
+		text: "1栋"
+	}, {
+		value: "302",
+		text: "2栋"
+	}, {
+		value: "303",
+		text: "3栋"
+	}, {
+		value: "304",
+		text: "4栋"
+	}]
+}, {
+	value: '400',
+	text: '加拿大花园',
+	children: [{
+		value: "401",
+		text: "1栋"
+	}, {
+		value: "402",
+		text: "2栋"
+	}, {
+		value: "403",
+		text: "3栋"
+	}, {
+		value: "404",
+		text: "4栋"
+	}]
+}]
