5b47ffea3e3279cc0bd7910e3112a67e84720618
249683003 <249683003@qq.com>
2017-06-20 15:06:03 +0800

    用户登录、绑定、解绑定实现

--- userservice.php
+++ userservice.php
@@ -1,6 +1,15 @@
 <?php
+include_once('dbconfig.php');
 class UserService{
 	private $ops = array('band'=>'band','login'=>'login','unband'=>'unband');
+	private $dbh;
+	public function __construct(){
+		try{
+			$dbh = new PDO($dsn,$user,$pass,array(PDO::ATTR_PERSISTENT => true));
+		}catch(PDOException $e){
+			die('error message '.$e->getMessage());
+		}
+	}
 	private function hasOps($op){
 		if(array_key_exists($op, $this->ops)){
 			return true;
@@ -13,7 +22,8 @@ class UserService{
 		if(!$this->hasOps($op)){
 			return false;
 		}
-		return $this->$op();
+		$handler = $this->ops[$op];
+		return $this->$handler();
 	}
 	private function bind(){
 		$name = isset($_POST['name'])?$_POST['name']:'';
@@ -41,7 +51,15 @@ class UserService{
 			return false;
 		}
 		//绑定业务
-		$res = array('code'=>0,'userinfo'=>array('uid'=>$uid,'name'=>$name,'password'=>$password,'linkwc'=>$openId));
+		$sql = 'INSERT INTO link VALUES(:openid,:uid)';
+		$stmt = $dbh->prepare($sql);
+		$rs = $stmt->execute(array(':openid'=>$openid,':uid'=>$uid));
+		if(!$rs){
+			$res = array('code'=>1,'msg'=>'band failed');
+		    echo json_encode($res);
+			return false;
+		}
+		$res = array('code'=>0,'userinfo'=>array('uid'=>$uid,'name'=>$name,'linkwc'=>$openId));
 		echo json_encode($res);
 		return true;
 	}
@@ -55,35 +73,56 @@ class UserService{
 			return false;
 		}
 		//解绑定业务
-		
+		$sql = 'DELETE FROM link WHERE openid = :openid';
+		$stmt = $dbh->prepare($sql);
+		$rs = $stmt->execute(array(':openid'=>$openid));
+		//解绑定失败
+		if(!$rs){
+			$res = array('code'=>1,'msg'=>'unband failed');
+		    echo json_encode($res);
+			return false;
+		}
+		//解绑定成功
+		$res = array('code'=>0,'msg'=>'unband success');
+		echo json_encode($res);
+		return true;
 	}
 	private function login(){
 		$name = isset($_POST['name'])?$_POST['name']:'';
 		$pwd = isset($_POST['password'])?$_POST['password']:'';
-		$openId = isset($_POST['openid'])?$_POST['openid']:'';
 		$res = array();
 		//判断参数是否齐全
 		if(empty($name)&&empty($pwd)){
-			if(!empty($openId)){
-				return $this->loginWithWc($openId);
-			}else{
 				$res = array('code'=>1,'msg'=>'lack of parameter');
 				echo json_encode($res);
 				return false;
-			}
 		}
 	    return loginNormal($name,$password);
 	}
-	private function loginWithWc($openId){
-		//用微信关联登录
-		
-	}
-	private function loginNormal($openId){
+	private function loginNormal($name,$password){
 		//用常规登录
-		
+		$cr = checkUser($name,$password);
+		if(!$cr[0]){
+			$res = array('code'=>1,'msg'=>'incorrect name or password ');
+			echo json_encode($res);
+			return false;
+		}
+		$_SESSION['uid'] = $cr[1];
+		$res = array('code'=>0,'uid'=>$cr[1]);
+		echo json_encode($res);
+		return false;
 	}
 	private function checkUser($name,$password){
-		
-		
+		$res = array();
+		$sql = 'SELECT * FROM account WHERE Name=:name AND PWD=:pwd';
+		$stmt = $dbh->prepare($sql);
+		$stmt->execute(array(':name'=>$name,':pwd'=>$password));
+		$rs = $stmt->fetch(PDO::FETCH_ASSOC);
+		if(empty($rs)){
+			$res[0]=false;
+			return $res;
+		}
+		$res[1] = $rs['ID'];
+		return $res;
 	}
 }
