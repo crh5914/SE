5689da5565d67b3440b452f1db6fd391a4b4de47
crh5914 <249683003@qq.com>
2017-06-21 11:49:22 +0800

    test

--- test.php
+++ test.php
@@ -1,25 +1,34 @@
 <?php
-//test code
-require_once('utils.php');
-require_once('service.php');
-require_once('db.php');
-//test get_access_token function
-//echo get_access_token();
-$tstr ="<xml>
-		<ToUserName><![CDATA[toUser]]></ToUserName>
-		<FromUserName><![CDATA[FromUser]]></FromUserName>
-		<CreateTime>123456789</CreateTime>
-		<MsgType><![CDATA[event]]></MsgType>
-		<Event><![CDATA[CLICK]]></Event>
-		<EventKey><![CDATA[123]]></EventKey>
-		</xml>";
-//dispatch_msg($tstr);
-$arr = array(
-'inarr'=>array('ass','2323')
-);
-foreach($arr['inarr'] as $value){
-	echo $value;
+class Test{
+	//数据访问对象
+	private $dsn;
+	private $user;
+	private $pwd;
+	private $opMap = array('band'=>'band','login'=>'login','query'=>'query');
+	//数据访问对象
+	public static $pdo;
+	public function __construct(){
+		$this->dsn = 'mysql:host=localhost;dbname=visit_assistant';
+		$this->user = 'root';
+		$this->pwd = 'root';
+		if(!isset(self::$pdo)){
+			try{
+				self::$pdo = new PDO($this->dsn,$this->user,$this->pwd,array(PDO::ATTR_PERSISTENT=>true));
+			}catch(PDOException $e){
+				die('error message '.$e->getMessage());
+			}
+		}
+	}
+    function query($uid,$park,$block){
+       $sql = 'SELECT * FROM buildingstructure WHERE Residentialarea_Name=:park';
+       $stmt = self::$pdo->prepare($sql);
+       $flag = $stmt->execute(array(':park'=>$park));
+       if($flag){
+       	  $rs = $stmt->fetchAll(PDO::FETCH_ASSOC);
+       	  echo json_encode($rs);
+       }
+    }
 }
-$db = new DB(array());
-var_dump($db->select('user',array('fields'=>array('name','age'),'where'=>array('name'=>'Nick'))));
+$t = new Test();
+$t->query('111', '海富花园', 'block')
 ?>
